151e0e47d0e4930cb62d44a109763f9acd6214c0
marklishman <mark.lishman@gmail.com>
2018-07-27 12:09:29 +0100

    Angular application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserListComponent } from './user/containers/user-list/user-list.component';
+import { UserViewComponent } from './user/components/user-view/user-view.component';
+import { UserEditComponent } from './user/components/user-edit/user-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/users', pathMatch: 'full'},
+  {path: 'users/:userId/edit', component: UserEditComponent},
+  {path: 'users/new', component: UserEditComponent},
+  {path: 'users/:userId', component: UserViewComponent},
+  {path: 'users', component: UserListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './main/app.component';
+import { UserListComponent } from './user/containers/user-list/user-list.component';
+import { UserViewComponent } from './user/components/user-view/user-view.component';
+import { UserEditComponent } from './user/components/user-edit/user-edit.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UserService } from './user/services/user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserViewComponent,
+    UserEditComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

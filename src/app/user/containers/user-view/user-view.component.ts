5affb1371bde5d69d1414ea42db3a015e5c6ccae
marklishman <mark.lishman@gmail.com>
2018-10-06 07:03:34 +0100

    Comment about router snapshot

--- src/app/user/containers/user-view/user-view.component.ts
+++ src/app/user/containers/user-view/user-view.component.ts
@@ -19,6 +19,7 @@ export class UserViewComponent implements OnInit {
               private route: ActivatedRoute) {}
 
   ngOnInit() {
+    // TODO this uses a snapshot. Should we be using this.route.queryParams.subscribe instead?
     const userId = +this.route.snapshot.paramMap.get('userId');
     this.user$ = this.userService.getUser$(userId);
   }

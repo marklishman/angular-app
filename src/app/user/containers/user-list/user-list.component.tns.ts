432698185de4e85ef5839709cda1e6581b30c92e
marklishman <mark.lishman@gmail.com>
2018-10-13 12:51:56 +0100

    dismissSoftInput

--- src/app/user/containers/user-list/user-list.component.tns.ts
+++ src/app/user/containers/user-list/user-list.component.tns.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Page } from 'tns-core-modules/ui/page';
+import { SearchBar } from "tns-core-modules/ui/search-bar";
 
 import { UserService } from '../../services/user.service';
 import { AbstractUserListComponent } from './abstract-user-list.component';
@@ -9,13 +10,22 @@ import { AbstractUserListComponent } from './abstract-user-list.component';
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
-export class UserListComponent extends AbstractUserListComponent {
+export class UserListComponent extends AbstractUserListComponent implements AfterViewInit {
+
+  @ViewChild("searchBar") searchBarEl: ElementRef;
+  searchBar: SearchBar;
 
   constructor(userService: UserService,
               router: Router,
               private page: Page) {
     super(userService, router);
     page.actionBarHidden = true;
+  }
+
+  ngAfterViewInit(): void {
+    this.searchBar = this.searchBarEl.nativeElement;
+    // TODO couldn't get this to work
+    this.searchBar.dismissSoftInput();
   }
 
   // TODO unit test
